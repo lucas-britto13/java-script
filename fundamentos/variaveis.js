d5b0307b15792fe67142e7a84ca4ceed54023a4d
lucas-britto13 <lucazz.britt0@gmail.com>
2026-04-08 22:24:07 -0300

    variáveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,44 @@
+/**
+ * Estudo das variáveis
+ * @author Lucas Britto 
+ */
+
+console.clear()
+console.log("Estudo das variáveis")
+console.log("")
+console.log("String")
+// a linha abaixo cria uma variável
+let nome = "Lucas Britto"
+console.log(nome)
+// a linha abaixo mostra o tipo de variável
+console.log(typeof(nome))
+console.log("Variaável do tipo Number")
+
+let idade = 57
+console.log(idade)
+console.log(typeof(idade))
+
+let peso = 70
+console.log(peso)
+console.log(typeof(idade))
+
+let altura = 1.74
+console.log(altura)
+console.log(typeof(altura))
+console.log("")
+let vip = true
+console.log(vip)
+console.log(typeof(vip))
+
+console.log("")
+console.log("------------------------------------")
+console.log("-         Ficha do aluno           -")
+console.log("------------------------------------")
+// A linha abaixo concatena (união) um texto
+// conteúdo da variável 
+console.log(`Nome: ${nome}`)
+console.log(`idade: ${idade}`)
+console.log(`peso: ${peso} kg de massa`)
+console.log(`altura: ${altura}`)
+console.log(`vip: ${vip}`)
+console.log("-----------------------------------")
